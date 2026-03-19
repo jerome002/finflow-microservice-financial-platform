a698acaa217b4dd42d7e7a0b3e02fa6641a1f32b
Jerome Kapkor Kimosop <jkapkor@gmail.com>
2026-03-19 15:11:25 +0300

    fix: import resendVerification in authRoutes

--- services/user-service/src/routes/authRoutes.js
+++ services/user-service/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, verifyEmail, forgotPassword, resetPasswordHandler } from "../controllers/authController.js";
+import { register, login, verifyEmail, forgotPassword, resetPasswordHandler,resendVerification} from "../controllers/authController.js";
 
 const router = express.Router();
 router.get("/health", (req, res) => res.status(200).json({ status: "UP" }));
